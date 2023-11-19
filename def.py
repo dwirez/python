7cf5e07d2b8b9eb062f4aca0b3a15f1a07f485a6
dwirez <dwirez.dwikireza64@gmail.com>
2023-11-19 22:32:52 +0700

    proses pembuatan fungsi owner

--- def.py
+++ def.py
@@ -2,35 +2,30 @@
 def buatakun():
     nama = input("Masukan Nama : " )
     paswd = input("Masukan Password : ")
-    user = {nama:paswd}
+    akun_karyawan = {nama:paswd}
     print("Akun Berhasil di buat")
-    return user
+    return akun_karyawan
 
-def Login(existing_user):
+def Login(owner,karyawan):
     print("Silahkan Login")
     pastikan = False
     while not pastikan :
         input_nama = input("masukan nama = ")
         input_passwd = input("masukan password = ")
         
-        if input_nama in existing_user and existing_user[input_nama] == input_passwd:
+        if input_nama in owner or karyawan and owner[input_nama] or karyawan[input_nama] == input_passwd:
             print("login Berhasil")
             pastikan = True
         else:
             print("Nama Pengguna atau sandi salah")
 
+def masuk_as_owner():
+    print("="*4,"Login Sebagai Owner", "="*4)
+    print("pilih yang ingin dilakukan\n1.buat akun karyawan\n2.lihat barang\n3.tambah barang\n4.histori transaksi")
+    milih = int(input("Masukan pilihan (1/2/3/4) : "))
+    if milih == 1 :
+        buatakun
 
-putusan = input("login or sign up : ")
-existing_user = {"dwiki" : "456"}
-
-while putusan != "login" and putusan != "sign up":
-    putusan = input(f"Salah input\nMasukan lagi Login or sign in : ")
-
-if putusan == "sign up" :
-    user_data = buatakun()
-    existing_user.update(user_data)
-    Login(existing_user)
-
-if putusan == "login":
-    Login(existing_user)
-
+putusan = input("login? (y/n) : ")
+if putusan == "y" :
+    Login(owner)
